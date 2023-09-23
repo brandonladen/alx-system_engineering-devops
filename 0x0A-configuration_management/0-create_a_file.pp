aa3777d3716180ed616d2b65bb56cb7a2992ed8a
Brandon Odhiambo Wamboga <95022425+brandonladen@users.noreply.github.com>
2023-09-23 13:24:24 +0300

    Update 0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,7 @@
-file { '/tmp/school':
-  ensure  => file,
-  mode    => '0744',
-  owner   => 'www-data',
-  group   => 'www-data',
-  content => 'I love Puppet',
-}
+# Creating A file in tmp folder named holberton
+file { '/tmp/holberton':
+    ensure  => 'present',
+    mode    => '0744',
+    owner   => 'www-data',
+    group   => 'www-data',
+    content => 'I love Puppet', }
