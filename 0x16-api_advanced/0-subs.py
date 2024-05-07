8290b0df4929b202193fcb9bf90b617172a96220
Brandon Odhiambo Wamboga <95022425+brandonladen@users.noreply.github.com>
2024-05-07 15:17:04 +0300

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,26 @@
 #!/usr/bin/python3
-"""Function to query subscribers on a given Reddit subreddit."""
+"""Quering Reddit"""
 
-import json
 import requests
 
 
 def number_of_subscribers(subreddit):
-     """query a subreddit and retrive no of subscribers"""
-    
-    user = {"User-Agent": "laden_brandon"}
-    request = requests.get("https://www.reddit.com/r/{}/about.json"
-                           .format(subreddit), headers=user)
-    if request.status_code == 200:
-        return request.json().get("data").get("subscribers")
+    """query a subreddit and retrive no of subscribers"""
+
+    # Reddit API endpoint for getting subreddit informatiom
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+
+    # Set a custom User-Agent to avoid too many requests error
+    headers = {'User-Agent': 'laden_brandon'}
+
+    # send a GET request to the Reddit API
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    # Chek if the request was successful and not redirect
+    if response.status_code == 200:
+        # parse JSON response to extract no of subscribers
+        data = response.json().get('data', {})
+        sub_count = data.get('subscribers', 0)
+        return sub_count
     else:
         return 0
