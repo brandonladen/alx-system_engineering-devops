a4e1a45e2c7c09b8b708eedb509efeb2d3c03e39
Brandon Odhiambo Wamboga <95022425+brandonladen@users.noreply.github.com>
2024-05-07 15:06:33 +0300

    Update2 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,7 +7,7 @@ import requests
 def number_of_subscribers(subreddit):
     """query a subreddit and retrive no of subscribers"""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': 'laden_brandon'}
+    headers = {'User-Agent': 'laden_brandon 1.0'}
     response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
