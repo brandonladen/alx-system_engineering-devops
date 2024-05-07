34bf6768abe37d2d92dccf073680a46627b43fdf
Brandon Odhiambo Wamboga <95022425+brandonladen@users.noreply.github.com>
2024-05-07 15:32:53 +0300

    Update 1-top_ten.py

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,29 +1,21 @@
 #!/usr/bin/python3
-"""Query subreddit"""
-
+"""Function to query subscribers on a given Reddit subreddit."""
+import json
 import requests
 
 
 def top_ten(subreddit):
-    """Query Reddit and print titles of the first 10 hot posts"""
-
-    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
-
-    # Set a custom User-Agent to avoid too many requests error
-    headers = {'User-Agent': 'My user Agent 1.0'}
-    params = {"limit": 10}
-
-    # send a GET request to the Reddit API
-    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
-
-    try:
-        response = requests.get(url, headers=headers,
-                                allow_redirects=False)
-        if response.status_code == 200:
-            children = response.json().get('data').get('children')
-            for i in range(10):
-                print(children[i].get('data').get('title'))
-        else:
-            print("None")
-    except Exception:
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+        "User-Agent": "laden_brandon"
+    }
+    params = {
+        "limit": 10
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code == 404:
         print("None")
+        return
+    results = response.json().get("data")
+    [print(c.get("data").get("title")) for c in results.get("children")]
