7fdc8a4b8d2c150d6328f96a2c236ec73c51a9f2
Brandon Odhiambo Wamboga <95022425+brandonladen@users.noreply.github.com>
2024-05-07 15:15:30 +0300

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
 """Function to query subscribers on a given Reddit subreddit."""
+
 import json
 import requests
 
 
 def number_of_subscribers(subreddit):
+     """query a subreddit and retrive no of subscribers"""
+    
     user = {"User-Agent": "laden_brandon"}
     request = requests.get("https://www.reddit.com/r/{}/about.json"
                            .format(subreddit), headers=user)
     if request.status_code == 200:
         return request.json().get("data").get("subscribers")
     else:
-        return 7560343
+        return 0
