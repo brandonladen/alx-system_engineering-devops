634ea8fe23d9b25d25065fc3ee0adba50c304447
brandonladen <brandonladen486@gmail.com>
2024-05-07 01:56:12 -0700

    updates

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -16,16 +16,16 @@ def recurse(subreddit, hot_list=[], after="", count=0):
         "count": count,
         "limit": 100
     }
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
+    response = requests.get(url, headers=headers, params=params, allow_redirects=False)
     if response.status_code == 404:
         return None
 
     results = response.json().get("data")
+    children = results.get("children")
+    hot_list += [child.get("data").get("title") for child in children]
+
     after = results.get("after")
     count += results.get("dist")
-    for c in results.get("children"):
-        hot_list.append(c.get("data").get("title"))
 
     if after is not None:
         return recurse(subreddit, hot_list, after, count)
